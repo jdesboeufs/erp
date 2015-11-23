4f2288be824b25de3a7abe461ff93aa534f2e1e3
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-11-23 14:05:49 +0100

    Add webpack

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 
 /* Mount assets */
 app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'assets', 'index.html'));
